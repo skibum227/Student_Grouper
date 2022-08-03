879fe3f3ae9df5d01b0b176fcf271cf1f4598082
skibum227 <jdh385@gmail.com>
2022-08-03 18:35:27 -0400

    cleaning up

--- app/app.py
+++ app/app.py
@@ -5,15 +5,35 @@ import secrets
 from grouper import Grouper
 from plotter import Plotter
 import pandas as pd
-# https://pythonbasics.org/flask-tutorial-templates/
-# https://stackoverflow.com/questions/53344797/how-create-an-array-with-checkboxes-in-flask
-# https://medium.com/geekculture/aws-container-services-part-1-b147e974c745
-# https://dev.to/marounmaroun/running-docker-container-with-gunicorn-and-flask-4ihg
-# Make it all look nice
-# https://dash-bootstrap-components.opensource.faculty.ai/docs/components/toast/
 
-app = Flask(__name__)
+import dash
+import dash_bootstrap_components as dbc
+import dash_daq as daq
+from dash import Input, Output, dcc, html
+
+import templates.styles as styles
+import templates.components as components
+
+################
+# APP DEFINITION
+################
+
+app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
+server = app.server
+
+###############
+# APP CONSTANTS
+###############
+
 name = 'Mrs. Herr'
+subtitle = 'The Student Grouper'
+stu_dict = pd.read_excel('student_ledger.xlsx', sheet_name=None)
+periods = list(stu_dict.keys())
+
+
+######################
+# HEALTH CHECK ENPONTS
+######################
 
 health = HealthCheck()
 envdump = EnvironmentDump()
@@ -22,57 +42,127 @@ def app_availible():
     return True, "App good to go!"
 
 def application_data():
-    return {"maintainer": "Skibum Woodworks",
+    return {"maintainer": "Skibum Woodworks Web Developement Division",
             "git_repo": "https://github.com/skibum227/Student_Grouper"}
 
 health.add_check(app_availible)
 envdump.add_section("application", application_data)
 
 # Add a flask route to expose information
-app.add_url_rule("/healthcheck", "healthcheck", view_func=lambda: health.run())
-app.add_url_rule("/environment", "environment", view_func=lambda: envdump.run())
+server.add_url_rule("/healthcheck", "healthcheck", view_func=lambda: health.run())
+server.add_url_rule("/environment", "environment", view_func=lambda: envdump.run())
 
-# This is where the app actually starts...
-@app.route('/')
-@app.route('/activity_parameters')
-def form(): 
-    return render_template('activity_params.html', title='Student Grouper', username=name)
 
-@app.route('/data', methods = ['POST', 'GET'])
-def data():
-    if request.method == 'GET':
-        return f"You are lost, got back to '/activity_parameters'"
+################
+# APP COMPONENTS
+################
 
-    if request.method == 'POST':
-        period = request.form.get('period')
-        group  = request.form.get('gps')
-        dl     = True if request.form.get('dl') else False
+# Build the sidebar 
+sidebar = components.sidebar_component(name, subtitle)
 
-        session['params'] = {'period': period, 'gps':int(group), 'distrib_lo': dl, 'filename': 'student_ledger.xlsx'}
+# Build the params content
+content_params = components.content_params_component(periods)
 
-        df = pd.read_excel('student_ledger.xlsx', sheet_name=f'period_{period}')
-        stu_dict = pd.Series(['y']*df.size,index=df.student_names).to_dict()
+# Build the roster content
+content_roster = components.content_roster_component()
 
-        return render_template('check_attendance.html', title='Student Grouper', stu_dict=stu_dict)
+# Build the table content
+content_table = components.content_table_component()
 
-@app.route('/attendance', methods=['POST', 'GET'])
-def check_attendance():
-    if request.method == 'GET':
-        return f"You are lost, got back to '/activity_parameters'"
 
-    if request.method == 'POST':
-        present_stus=request.form.getlist('pres')
-        grouper = Grouper(session['params'], present_stus)
+###############
+# Construct App
+###############
 
-        df = grouper.group_students()
+# This puts the whole damn thing together
+app.layout = html.Div([dcc.Location(id="ip"), sidebar, content_params, content_roster, content_table])
 
-        grouper.print_student_groups(df)
 
-        graphjson = Plotter(session['params'], df).plot_groups()
-        return render_template('plot_student_groups.html', graphjson=graphjson)
+@app.callback(
+     Output(component_id="student_roster", component_property="children"),
+     Output(component_id="page-content_three", component_property="children"),
+     Output(component_id="page-content_one", component_property="style"),
+     Output(component_id="student_roster_page", component_property="style"),
+     Output(component_id="page-content_three", component_property="style"),
+
+     Input(component_id="ip", component_property="pathname"),
+     Input(component_id="period_selection", component_property="value"),
+     Input(component_id="group_size", component_property="value"),
+     Input(component_id="distribute_leftovers", component_property="value"),
+     Input(component_id="student_roster", component_property="children")
+)
+def render_page_content(pathname, period_selection, group_size, distribute_leftovers, student_roster):
+
+    # Generate the df for only the partiuclar period 
+    df = pd.DataFrame(stu_dict[period_selection]).sort_values('student_names')
+    # The list of student names
+    student_names = df['student_names'].values.tolist()
+
+    if pathname in ["/parameters", "/"]: # Left the old one on purpose
+        return (
+                    components.roster_builder(student_names), \
+                    None, \
+                    styles.CONTENT_STYLE_ON, \
+                    styles.CONTENT_STYLE_OFF, \
+                    styles.CONTENT_STYLE_OFF
+               )
+
+    elif pathname in ("/roster", "/groups"):
+        print(student_roster)
+        keys = [x['props']['children'][0]['props']['children']['props']['id'] for x in student_roster]
+        values = [x['props']['children'][0]['props']['children']['props']['value'] for x in student_roster]
+        df = pd.DataFrame({k:v for (k,v) in zip(keys, values)}.items(), columns=['student_names', 'present']).sort_values('student_names')
+        df = df[df.present.eq(True)]
+
+        if pathname == "/roster":
+            return (
+                        components.roster_builder(df.student_names.to_list()), \
+                        None, \
+                        styles.CONTENT_STYLE_OFF, \
+                        styles.CONTENT_STYLE_ON, \
+                        styles.CONTENT_STYLE_OFF
+                   )
+
+        elif pathname == "/groups":
+
+            distrib_lo = True if distribute_leftovers == 1 else False
+            params = {'student_df': df, 'period': period_selection, 'gps':group_size, 'distrib_lo': distrib_lo, 'filename': 'student_ledger.xlsx'} 
+            grouper = Grouper(params)
+            df_gps = grouper.group_students()
+
+            params = {'period': '1', 'gps':3, 'distrib_lo': False, 'filename': 'student_ledger.xlsx', 'dont_plot': False}
+            fig = Plotter(params, df_gps).plot_groups()
+
+            return (
+                        components.roster_builder(df.student_names.to_list()), \
+                        dcc.Graph(figure=fig), \
+                        styles.CONTENT_STYLE_OFF, \
+                        styles.CONTENT_STYLE_OFF, \
+                        styles.CONTENT_STYLE_ON
+                   )
+
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
 
 secret = secrets.token_urlsafe(32)
 app.secret_key = secret
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050)
+    app.run(host='0.0.0.0', port=5050, debug=True)
+
+
+
+
+
+
+
+
+
+
+

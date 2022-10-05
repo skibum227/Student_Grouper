585c4ba3ee3b9eec7b8365bbaa4eefd7ce942ff0
skibum227 <jdh385@gmail.com>
2022-10-05 07:58:27 -0400

    formatting

--- infa/cloudwatch_schedule.tf
+++ infa/cloudwatch_schedule.tf
@@ -13,42 +13,16 @@ resource "aws_cloudwatch_event_rule" "lambda_stop" {
 }
 
 resource "aws_cloudwatch_event_target" "lambda_start" {
-  rule      = aws_cloudwatch_event_rule.lambda_start
-  target_id = "Send start msg"
-  arn       = aws_lambda_function.lambda_ecs_control.arn 
-  input_transformer {
-    input_paths = {
-      status  = "start"
-      cluster = "jobs-cluster",
-      service = "student-grouper-application-v3",
-    }
-    input_template = <<EOF 
-    {
-        "status":  <status>,
-        "cluster": <cluster>,
-        "service": <service>,
-    }
-    EOF
-  }
+  rule      = aws_cloudwatch_event_rule.lambda_start.id
+  target_id = "send_start_msg"
+  arn       = aws_lambda_function.lambda_ecs_control.arn
+  input     = "{\"status\":\"start\", \"cluster\":\"jobs-cluster\", \"service\":\"student-grouper-application-v3\"}"
 }
 
 resource "aws_cloudwatch_event_target" "lambda_stop" {
-  rule      = aws_cloudwatch_event_rule.lambda_stop
-  target_id = "Send start msg"
+  rule      = aws_cloudwatch_event_rule.lambda_stop.id
+  target_id = "send_stop_msg"
   arn       = aws_lambda_function.lambda_ecs_control.arn
-  input_transformer {
-    input_paths = {
-      status  = "stop"
-      cluster = "jobs-cluster",
-      service = "student-grouper-application-v3",
-    }
-    input_template = <<EOF
-    {
-        "status":  <status>,
-        "cluster": <cluster>,
-        "service": <service>,
-    }
-    EOF
-  }
+  input     = "{\"status\":\"stop\", \"cluster\":\"jobs-cluster\", \"service\":\"student-grouper-application-v3\"}"
 }
 
--- infa/permissions.tf
+++ infa/permissions.tf
@@ -21,8 +21,8 @@ resource "aws_iam_role" "task_resource_access" {
 # Below is all for lambda
 data "aws_iam_policy_document" "AWSLambdaTrustPolicy" {
   statement {
-    actions    = ["sts:AssumeRole"]
-    effect     = "Allow"
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
     principals {
       type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
@@ -32,13 +32,13 @@ data "aws_iam_policy_document" "AWSLambdaTrustPolicy" {
 
 resource "aws_iam_role" "lambda_role" {
   name               = "${var.resource_prefix}-lambda-ecs-control-role"
-  assume_role_policy = "${data.aws_iam_policy_document.AWSLambdaTrustPolicy.json}"
+  assume_role_policy = data.aws_iam_policy_document.AWSLambdaTrustPolicy.json
   managed_policy_arns = [
     "arn:aws:iam::aws:policy/AmazonECS_FullAccess",
   ]
 }
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-  role       = "${aws_iam_role.lambda_role.name}"
+  role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }

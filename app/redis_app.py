b27c4febb42bdf8519aa8d42267e063dc64e749c
skibum227 <jdh385@gmail.com>
2022-10-27 08:09:56 -0400

    cleaning up

--- app/redis_app.py
+++ app/redis_app.py
@@ -1,211 +1,53 @@
-from flask import Flask, request, render_template, session
-from healthcheck import HealthCheck, EnvironmentDump
-
-import secrets
-from grouper import Grouper
-from plotter import Plotter
-import pandas as pd
-
+# All Dash Imports
 import dash
+from dash import (Input,
+                 Output,
+                 State,
+                 dcc,
+                 html,
+                 ctx,
+                 dash_table)
 import dash_bootstrap_components as dbc
-import dash_daq as daq
-from dash import Input, Output, dcc, html, ctx
-
 import templates.styles as styles
-import templates.components as components
 
-#------
-
-# for reading in a file
+# All other imports
 import base64
-import datetime
 import io
-
-from dash.dependencies import Input, Output, State
-from dash import dash_table
-# ----
-
-# for redis
-import redis
 import json
-
 import pandas as pd
-# Notes
-# Want to use this: Offcanvas and start with loading/deleting a whole class
-
-#https://dash-bootstrap-components.opensource.faculty.ai/docs/components/table/
-#https://dash.plotly.com/basic-callbacks#dash-app-with-chained-callbacks
+import redis
+import secrets
 
 ################
 # APP DEFINITION
 ################
 
-# app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
 secret = secrets.token_urlsafe(32)
 app.secret_key = secret
 server = app.server
 
-###############
-# APP Constants
-###############
+#############
+# APP Globals
+#############
 
-class_names = {'0':'Period 1', '1':'Period 2', '2':'Period 3', '3':'Period 4', '4':'Period 5', '5':'Period 6'}
-r = redis.StrictRedis(host='127.0.0.1',port='6379',db=0,charset="utf-8",decode_responses=True)
+# All possible classes at PV
+all_class_names = {'0':'Period 1', '1':'Period 2', '2':'Period 3', '3':'Period 4', '4':'Period 5', '5':'Period 6'}
+# Connection to db for read/write
+database = redis.StrictRedis(host='127.0.0.1',port='6379',db=0,charset="utf-8",decode_responses=True)
+
+# Displays styles for on/off switing
+on_style =  {'margin': '10px', 'width': '95%', 'display':'block'}
+off_style = {'margin': '10px', 'width': '95%', 'display':'None'}
 
 def current_availible_classes():
-    r = redis.StrictRedis(host='127.0.0.1',port='6379',db=0,charset="utf-8",decode_responses=True)
-    sorted_keys = r.keys()
+    # Simply returns all possible classes for initialization of app
+    sorted_keys = database.keys()
     sorted_keys.sort()
-    return [{'value':x, 'label':class_names[x]} for x in sorted_keys]
-
-on_style =  {'margin': '10px', 'width': '100%', 'display':'block'}
-off_style = {'margin': '10px', 'width': '100%', 'display':'None'}
-
-###############
-# APP Layout
-###############
-
-df = pd.DataFrame()
-app.layout = html.Div(
-    dbc.Row([
-        dbc.Button("Open Upload", id="open-offcanvas-upload", n_clicks=0),
-        dbc.Button("Open Adjust", id="open-offcanvas-adjust", n_clicks=0),
-        dbc.Offcanvas([
-            dbc.Col([
-                html.H3(
-                    "Upload Class",
-                    className="my-2",
-                    style={'margin': '10px', 'width': '100%', 'textAlign': 'center'}
-                ),
-                html.Hr(style={'margin': '10px', 'width': '100%'}),
-                dbc.Select(
-                    id="upload-a-class",
-                    options=[{"label": v, "value": k} for k,v in class_names.items()],
-                    style={'margin': '10px', 'width': '100%'}
-                ),
-                dcc.Upload(
-                    id='upload-data',
-                    children=html.Div([
-                        dbc.Button(
-                            "Drag and Drop or Select Files",
-                            outline=True,
-                            color='primary',
-                            style={'margin': '10px', 'width': '100%'}
-                        )
-                    ]),
-                ),
-                html.Br(),
-                html.Hr(
-                    style={'margin': '10px', 'width': '100%'}
-                ),
-                dbc.Button(
-                    'Submit Period Name and Roster',
-                    id='submit-upload',
-                    className="me-2",
-                    style={'margin': '10px', 'width': '100%'},
-                    n_clicks=0
-                ),
-                html.P(
-                    id='upload-confirm',
-                    className="my-2",
-                    style={'margin': '10px', 'width': '100%', 'textAlign': 'center'}
-                ),
-            ]), #, width={'size':2, 'offset': 1}),
-            dbc.Col([
-                html.Div(id='data-to-upload', style={'margin': '10px', 'width': '100%'}),
-            ]),#, width={'size':2, 'offset': 0}),
-        ], id="offcanvas-upload", title="Title", is_open=False, placement='end'),
-        dbc.Offcanvas([
-            dbc.Col([
-                html.H3(
-                    "Select Class",
-                    className="my-2",
-                    style={'margin': '10px', 'width': '100%', 'textAlign': 'center'}
-                ),
-                html.Hr(style={'margin': '10px', 'width': '100%'}),
-                dbc.Select(
-                    id="select-a-class",
-                    options=current_availible_classes(),
-                    style={'margin': '10px', 'width': '100%'}
-                ),
-                dbc.RadioItems(
-                    id='adjust-select',
-                    options=[
-                        {"label": "Add Student", "value": 1},
-                        {"label": "Remove Student", "value": 2},
-                        {"label": "Delete Class", "value": 3},
-                    ],
-                    value=1,
-                    style=on_style,
-                ),
-                html.Br(),
-                html.Hr(
-                    style={'margin': '10px', 'width': '100%'}
-                ),
-                dbc.Input(
-                    id='student-name',
-                    placeholder="Enter New Student's Name...", 
-                    type="text",
-                    style=off_style,
-                ),
-                dbc.Button(
-                    'Add Student',
-                    id='add-student',
-                    color='success',
-                    className="me-1",
-                    style=off_style,
-                    n_clicks=0
-                ),
-                dbc.Select(
-                    id="select-a-student",
-                    options=[],
-                    style=off_style,
-                ),
-                dbc.Button(
-                    'Delete Student',
-                    id='delete-student',
-                    color='warning',
-                    className="me-1",
-                    style=off_style,
-                    n_clicks=0
-                ),
-                dbc.Button(
-                    'Delete Class',
-                    id='delete-class',
-                    color='danger',
-                    className="me-1",
-                    style=off_style,
-                    n_clicks=0
-                ),
-                html.P(
-                    id='action-confirm',
-                    className="my-2",
-                    style={'margin': '10px', 'width': '100%', 'textAlign': 'center'}
-                ),
-            ]),#, width={'size':2, 'offset': 0}),
-        ], id="offcanvas-adjust", title="Title", is_open=False, placement='end'),
-    ])
-)
-
-@app.callback(
-    Output("offcanvas-upload", "is_open"),
-    Output("offcanvas-adjust", "is_open"),
-    Input("open-offcanvas-upload", "n_clicks"),
-    Input("open-offcanvas-adjust", "n_clicks"),
-    [State("offcanvas-upload", "is_open"),
-     State("offcanvas-adjust", "is_open")],
-)
-def toggle_offcanvas(n1, n2, is_open_upload, is_open_adjust):
-    if ctx.triggered_id == 'open-offcanvas-upload':
-        return not is_open_upload, is_open_adjust
-    elif ctx.triggered_id == 'open-offcanvas-adjust':
-        return is_open_upload, not is_open_adjust
-    else:
-        return is_open_upload, is_open_adjust
-
+    return [{'value':x, 'label':all_class_names[x]} for x in sorted_keys]
 
 def parse_contents(contents, filename):
+    # Read in differetn formats for class roster data
     content_type, content_string = contents.split(',')
 
     decoded = base64.b64decode(content_string)
@@ -213,11 +55,9 @@ def parse_contents(contents, filename):
         if 'csv' in filename:
             # Assume that the user uploaded a CSV file
             df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
-
         elif 'xls' in filename:
             # Assume that the user uploaded an excel file
             df = pd.read_excel(io.BytesIO(decoded))
-
     except Exception as e:
         print(e)
         return html.Div([
@@ -225,29 +65,169 @@ def parse_contents(contents, filename):
         ])
     return df
 
-
-def save_to_redis(df, name):
+def save_to_database(df, name):
+    # Takes input value and saves to the database with key
     data = json.dumps(df.student_names.tolist())
-    r[name] = data
-  
+    database[name] = data
 
 def build_roster_table(df):
-    # Create the header for the table
+    # Creates the output table for the uploaded class
     table_header = [html.Thead(html.Tr([html.Th("Class Roster")]))]
-    # Create tehe body of the tabl
     table_body = [html.Tbody([html.Tr([html.Td(x)]) for x in df.student_names.to_list()])]
-    # Create the table
     table = dbc.Table(
         table_header + table_body,
-        bordered=True,
-        dark=True,
-        hover=True,
-        responsive=True,
-        striped=True
+        bordered=True, dark=True, hover=True, responsive=True, striped=True,
     )
     return table
 
+############
+# APP Layout
+############
 
+df = pd.DataFrame()
+app.layout = html.Div([
+    dbc.Button("Open Upload", id="offcanvas-upload-btn", n_clicks=0, style={'margin': '10px'}),
+    dbc.Button("Open Adjust", id="offcanvas-adjust-btn", n_clicks=0, style={'margin': '10px'}),
+    dbc.Button("Download Roster", id='download_btn', href="data_file.txt", color="primary", style={'margin': '10px'}),
+    dcc.Download(id="download"), # this just enables the download to occur, nothing is displayed
+    dbc.Offcanvas([
+        html.H3(
+            "Upload Class",
+            className="my-2",
+            style={'margin': '10px', 'width': '95%', 'textAlign': 'center'}
+        ),
+        html.Hr(style={'margin': '10px', 'width': '95%'}),
+        dbc.Select(
+            id="upload-a-class",
+            options=[{"label": v, "value": k} for k,v in all_class_names.items()],
+            style={'margin': '10px', 'width': '95%'}
+        ),
+        dcc.Upload(
+            id='upload-data',
+            children=html.Div([
+                dbc.Button(
+                    "Drag and Drop or Select Files",
+                    outline=True,
+                    color='primary',
+                    style={'margin': '10px', 'width': '95%'}
+                )
+            ]),
+        ),
+        html.Br(),
+        html.Hr(
+            style={'margin': '10px', 'width': '95%'}
+        ),
+        dbc.Button(
+            'Submit Period Name and Roster',
+            id='submit-upload',
+            className="me-2",
+            style={'margin': '10px', 'width': '95%'},
+            n_clicks=0
+        ),
+        html.P(
+            id='upload-confirm',
+            className="my-2",
+            style={'margin': '10px', 'width': '95%', 'textAlign': 'center'}
+        ),
+        html.Div(id='data-to-upload', style={'margin': '10px', 'width': '100%'}),
+    ], id="offcanvas-upload", title="Roster Database", is_open=False, placement='end', backdrop=True),
+    dbc.Offcanvas([
+        html.H3(
+            "Select Class",
+            className="my-2",
+            style={'margin': '10px', 'width': '95%', 'textAlign': 'center'}
+        ),
+        html.Hr(style={'margin': '10px', 'width': '95%'}),
+        dbc.Select(
+            id="select-a-class",
+            options=current_availible_classes(),
+            style={'margin': '10px', 'width': '95%'}
+        ),
+        dbc.RadioItems(
+            id='adjust-select',
+            options=[
+                {"label": "Add Student", "value": 1},
+                {"label": "Remove Student", "value": 2},
+                {"label": "Delete Class", "value": 3},
+            ],
+            value=1,
+            style=on_style,
+        ),
+        html.Br(),
+        html.Hr(
+            style={'margin': '10px', 'width': '95%'}
+        ),
+        dbc.Input(
+            id='student-name',
+            placeholder="Enter New Student's Name...",
+            type="text",
+            style=off_style,
+        ),
+        dbc.Button(
+            'Add Student',
+            id='add-student',
+            color='success',
+            className="me-1",
+            style=off_style,
+            n_clicks=0
+        ),
+        dbc.Select(
+            id="select-a-student",
+            options=[],
+            style=off_style,
+        ),
+        dbc.Button(
+            'Delete Student',
+            id='delete-student',
+            color='warning',
+            className="me-1",
+            style=off_style,
+            n_clicks=0
+        ),
+        dbc.Button(
+            'Delete Class',
+            id='delete-class',
+            color='danger',
+            className="me-1",
+            style=off_style,
+            n_clicks=0
+        ),
+        html.P(
+            id='action-confirm',
+            className="my-2",
+            style={'margin': '10px', 'width': '95%', 'textAlign': 'center'}
+        ),
+    ], id="offcanvas-adjust", title="Roster Database", is_open=False, placement='end', backdrop=True),
+])
+
+# Download all roster data to csv (need to finish!)
+@app.callback(
+    Output("download", "data"),
+    Input("download_btn", "n_clicks")
+)
+def generate_csv(n_nlicks):
+    df = pd.DataFrame()
+    if ctx.triggered_id == 'dwnld_btn':
+        return dcc.send_data_frame(df.to_csv, filename="full_roster.csv")
+
+# Open/Close the offcanvas objects
+@app.callback(
+    Output("offcanvas-upload", "is_open"),
+    Output("offcanvas-adjust", "is_open"),
+    Input("offcanvas-upload-btn", "n_clicks"),
+    Input("offcanvas-adjust-btn", "n_clicks"),
+    [State("offcanvas-upload", "is_open"),
+     State("offcanvas-adjust", "is_open")],
+)
+def toggle_offcanvas(n1, n2, is_open_upload, is_open_adjust):
+    if ctx.triggered_id == 'offcanvas-upload-btn':
+        return not is_open_upload, is_open_adjust
+    elif ctx.triggered_id == 'offcanvas-adjust-btn':
+        return is_open_upload, not is_open_adjust
+    else:
+        return is_open_upload, is_open_adjust
+
+# Control the upload roster object
 @app.callback(
     Output('data-to-upload', 'children'),
     Output('upload-confirm', 'children'),
@@ -264,8 +244,8 @@ def update_output(contents, filename,  n_clicks, value):
         roster_table = build_roster_table(roster_df)
 
         if ctx.triggered_id == 'submit-upload' and value:
-            save_to_redis(roster_df, value)
-            msg = f'{class_names[value]} has been saved'
+            save_to_database(roster_df, value)
+            msg = f'{all_class_names[value]} has been saved'
         else:
             msg = ''
 
@@ -273,29 +253,29 @@ def update_output(contents, filename,  n_clicks, value):
 
     return None, None
 
-
+# Enables selecting a class to update
 @app.callback(
     Output('select-a-class', 'options'),
     Input('select-a-class', 'value')
 )
 def get_loaded_classes(value):
 
-    sorted_keys = r.keys()
+    sorted_keys = database.keys()
     sorted_keys.sort()
-    return [{'value':x, 'label':class_names[x]} for x in sorted_keys]
+    return [{'value':x, 'label':all_class_names[x]} for x in sorted_keys]
 
-
+# Controls which student is selected for removal
 @app.callback(
     Output('select-a-student', 'options'),
     Input('select-a-class', 'value')
 )
 def get_student_names(class_index):
     if class_index:
-        roster = json.loads(r.get(class_index))
+        roster = json.loads(database.get(class_index))
         return [{"label": x, "value": f'{i}'} for i,x in enumerate(roster)]
     return []
 
-
+# Changes which adjustment options are shown dependent on the selector
 @app.callback(
     Output('student-name', 'style'),
     Output('add-student', 'style'),
@@ -315,9 +295,9 @@ def display_actions(action, class_name):
     else:
         return off_style, off_style, off_style, off_style, off_style
 
-
+# Handles all actions within selector options
 @app.callback(
-    Output('action-confirm', 'children'), 
+    Output('action-confirm', 'children'),
     Input('select-a-class', 'value'),
     Input('student-name', 'value'),
     Input('select-a-student', 'value'),
@@ -329,31 +309,31 @@ def make_changes(class_value, student_name, student_value, n_clicks_as, n_clicks
 
     if ctx.triggered_id == 'add-student' and class_value is not None and student_name is not None:
         # Get the roster
-        roster = json.loads(r.get(class_value))
+        roster = json.loads(database.get(class_value))
         # Add the name
         roster.append(student_name)
         # Save it!
-        r[class_value] = json.dumps(roster)
+        database[class_value] = json.dumps(roster)
 
-        msg = f'{student_name} has been added to {class_names[class_value]}!'
+        msg = f'{student_name} has been added to {all_class_names[class_value]}!'
 
     elif ctx.triggered_id == 'delete-student' and class_value is not None and student_value is not None:
         # Get the roster
-        roster = json.loads(r.get(class_value))
+        roster = json.loads(database.get(class_value))
         # Remove the name
         name = roster[int(student_value)]
         roster.pop(int(student_value))
         # Save it!
-        r[class_value] = json.dumps(roster)
+        database[class_value] = json.dumps(roster)
 
-        msg = f'{name} has been removed from {class_names[class_value]}!'
+        msg = f'{name} has been removed from {all_class_names[class_value]}!'
 
     elif ctx.triggered_id == 'delete-class' and class_value is not None:
-        r.delete(class_value)
-        msg = f'{class_names[class_value]} has been deleted!'
+        database.delete(class_value)
+        msg = f'{all_class_names[class_value]} has been deleted!'
     else:
         msg = ''
-    return msg   
+    return msg
 
 
 if __name__ == "__main__":
